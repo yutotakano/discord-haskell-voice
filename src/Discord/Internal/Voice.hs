67c7fd7394a5db7e56dcb01e898f707682d889f9
Yuto Takano <moa17stock@gmail.com>
2021-05-02 18:04:28 +0100

    Complete a lot of the Websocket event loops and threading

--- src/Discord/Internal/Voice.hs
+++ src/Discord/Internal/Voice.hs
@@ -30,8 +30,7 @@ data DiscordVoiceThreadId = DiscordVoiceThreadIdGateway ThreadId
 type DiscordVoiceHandleGateway = (Chan (Either GatewayException Event), Chan GatewaySendable)
 
 data DiscordVoiceHandle = DiscordVoiceHandle
-    { discordVoiceHandleGateway     :: DiscordHandleGateway
-    , discordVoiceHandleWebsocket   :: DiscordVoiceHandleWebsocket
+    { discordVoiceHandleWebsocket   :: DiscordVoiceHandleWebsocket
     , discordVoiceHandleUDP         :: DiscordVoiceHandleUDP
     , discordVoiceThreads           :: [DiscordVoiceThreadId]
     }
@@ -42,7 +41,6 @@ joinVoice gid cid mute deaf = do
     -- Duplicate the event channel, so we can read without taking data from event handlers
     h <- ask
     (_events, _, _) <- discordHandleGateway h
-
     events <- dupChan _events
     
     -- Send opcode 4
@@ -56,20 +54,35 @@ joinVoice gid cid mute deaf = do
     -- Loop for a maximum of 5 seconds
     result <- liftIO loopUntilEvents
     case result of
-        Left e                   ->
+        Nothing ->
             liftIO $ writeChan (discordHandleLog h) "Discord did not respond to opcode 4 in time."
-        Right (_, _, _, Nothing) ->
+        Just (_, _, _, Nothing) ->
             liftIO $ writeChan (discordHandeLog h) "Discord did not give a good endpoint."
-        Right connData ->
-            liftIO $ startVoiceThreads connData
-            
+        Just (sessionId, token, guildId, Just endpoint) -> do
+            let connData = (sessionId, token, guildId, endpoint) :: WebsocketConnData
+            -- Get the current user ID, and pass it on with all the other data
+            eCache <- liftIO $ readMVar $ discordHandleCache h :: (Either (Cache, GatewayException) Cache)
+            case eCache of
+                Left _ ->
+                    liftIO $ writeChan (discordHandeLog h) "Could not get current user."
+                Right cache ->
+                    let user = _currentUser cache
+                    let userId = userId user
+
+                    liftIO $ startVoiceThreads connData userId $ discordHandleLog h
+
 -- | Loop a maximum of 5 seconds, or until both Voice State Update and Voice Server Update has been received
 loopUntilEvents :: Chan (Either GatewayException Event) -> IO (Maybe (T.Text, T.Text, GuildId, Maybe T.Text))
-loopUntilEvents events = eitherRight <$> race (threadDelay (5 * 10^(6 :: Int))) waitForVoiceState
+loopUntilEvents events = eitherRight <$> race (threadDelay (5 * 10^(6 :: Int))) waitForBoth
   where
-    -- | Wait for the VOICE_STATE_UPDATE event
-    waitForVoiceState :: IO (T.Text, T.Text, GuildId, Maybe T.Text)
-    waitForVoiceState = do
+    waitForBoth :: IO (T.Text, T.Text, GuildId, Maybe T.Text)
+    waitForBoth = do
+        waitForBoth' Nothing Nothing
+
+    -- | Wait for both VOICE_STATE_UPDATE and VOICE_SERVER_UPDATE, order is undefined in docs
+    waitForBoth' :: Maybe T.Text -> Maybe (T.Text, GuildId, Maybe T.Text) -> IO (T.Text, T.Text, GuildId, Maybe T.Text)
+    waitForBoth' (Just a) (Just (b, c, d) = pure $ (a, b, c, d)
+    waitForBoth' mb1 mb2 = do
         top <- readChan events
         case top of
             Right (UnknownEvent "VOICE_STATE_UPDATE" obj) -> do
@@ -78,35 +91,38 @@ loopUntilEvents events = eitherRight <$> race (threadDelay (5 * 10^(6 :: Int))) 
                 let sessionId = fromJust $ parseMaybe $ do
                         d <- obj .: "d"
                         d .: "session_id"
-                waitForVoiceServer sessionId
-            Right _ -> waitForVoiceState
-            Left _  -> waitForVoiceState
-
-    -- | Wait for the VOICE_SERVER_UPDATE event
-    waitForVoiceServer :: T.Text -> IO (T.Text, T.Text, GuildId, Maybe T.Text)
-    waitForVoiceServer sessionId = do
-        top <- readChan events
-        case top of
+                waitForBoth' (Just sessionId) mb2
             Right (UnknownEvent "VOICE_SERVER_UDPATE" obj) -> 
-                fromJust $ parseMaybe $ do
+                let result = fromJust $ parseMaybe $ do
                    d <- obj .: "d"
                    token <- d .: "token"
                    guildId <- d .: "guildId"
                    endpoint <- d .: "endpoint"
-                   pure $ (sessionId, token, guildId, Just $ endpoint)
-            Right _ -> waitForVoiceServer
-            Left _  -> waitForVoiceServer
-   
+                   pure $ (token, guildId, endpoint)
+                waitForBoth' mb1 (Just result)
+            Right _ -> waitForBoth' mb1 mb2
+            Left _  -> waitForBoth' mb1 mb2
+
 -- | Selects the right element as a Maybe
 eitherRight :: Either a b -> Maybe b
 eitherRight (Left _)  = Nothing
 eitherRight (Right x) = Just x
 
 -- | Start the Websocket thread, which will create the UDP thread
-startVoiceThreads :: IO DiscordVoiceHandle
-startVoiceThreads = do
+startVoiceThreads :: WebsocketConnData -> UserId -> Chan T.Text -> IO DiscordVoiceHandle
+startVoiceThreads connData uid log = do
     -- The UDP thread is not created immediately, so what is returned is
     -- an MVar to the data. We will wait and read from it.
-    (websocket, websocketId, mUdp, mUdpId) <- forkIO $ voiceWebsocketLoop
+    events <- newChan :: IO (Chan (Either VoiceWebsocketException Event))
+    sends <- newChan
+    websocketId <- forkIO $ voiceWebsocketLoop (events, sends) (connData uid) log
+
+    pure $ DiscordVoiceHandle
+        { discordVoiceHandleWebsocket = (events, sends)
+        , discordVoiceThreads =
+            [ websocketId
+            , udpId
+            ]
+        }
      
 
--- src/Discord/Internal/Voice/EventLoop.hs
+++ src/Discord/Internal/Voice/EventLoop.hs
@@ -1,7 +1,23 @@
 module Discord.Internal.Voice.EventLoop where
 
+import           Control.Exception.Safe     ( try
+                                            , SomeException
+                                            )
+import           Control.Monad              ( forever
+                                            )
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
-import           Wuss               ( runSecureClient )
+import qualified Data.Text.Encoding as TE
+import           Data.Time.Clock.POSIX
+import           Wuss                       ( runSecureClient )
+import           Network.Websockets         ( ConnectionException(..)
+                                            , Connection
+                                            , receiveData
+                                            , sendTextData
+                                            )
+
+import           Discord.Internal.Types
+import           Discord.Internal.Types.VoiceWebsocket
 
 data VoiceWebsocketException = VoiceWebsocketCouldNotConnect T.Text
                              | VoiceWebsocketEventParseError T.Text
@@ -9,5 +25,170 @@ data VoiceWebsocketException = VoiceWebsocketCouldNotConnect T.Text
                              | VoiceWebsocketConnection ConnectionException T.Text
                              deriving (Show)
 
+type DiscordVoiceHandleWebsocket = (Chan (Either VoiceWebsocketException VoiceWebsocketReceivable), Chan VoiceWebsocketSendable)
 
-type DiscordHandleVoice = (Chan (Either VoiceWebsocketException Event), Chan VoiceWebsocketSendable)
+-- | session_id, token, guild_id, endpoint
+type WebsocketConnInfo = ConnInfo
+    { sessionId  :: T.Text
+    , token      :: T.Text
+    , guildId    :: GuildId
+    , endpoint   :: T.Text
+    }
+
+type WebsocketConn = ConnData
+    { connection :: Connection
+    , connInfo   :: WebsocketConnInfo
+    , eventsChan :: Chan (Either VoiceWebsocketException VoiceWebsocketReceivable)
+    }
+
+data ConnLoopState
+    = ConnStart
+    | ConnClosed
+    | ConnReconnect
+    deriving Show
+
+connect :: T.Text -> (Connection -> IO a) -> IO a
+connect url = runSecureClient url 443 ""
+
+voiceWebsocketLoop :: DiscordVoiceHandleWebsocket -> (WebsocketConnInfo, UserId) -> Chan T.Text -> IO ()
+voiceWebsocketLoop (receives, sends) (info, userId) log = loop ConnStart 0
+  where
+    loop :: ConnLoopState -> Int -> IO ()
+    loop s retries = do
+        case s of
+            ConnClosed -> pure ()
+
+            ConnStart -> do
+                next <- try $ connect (endpoint info) $ \conn -> do
+                    -- Send opcode 0 Identify
+                    sendTextData conn $ encode $ Identify $ IdentifyPayload
+                        { identifyPayloadServerId = guildId info
+                        , identifyPayloadUserId = userId
+                        , identifyPayloadSessionId = sessionId info
+                        , identifyPayloadToken = token info
+                        }
+                    -- Attempt to get opcode 2 Ready
+                    msg <- getPayload conn log
+                    case msg of
+                        -- Not documented in the docs, but we presume Identify -> Ready is done before Hello
+                        Right (Ready payload) -> do
+                            -- Ready received, so now wait for Hello
+                            msg2 <- getPayload conn log
+                            case msg2 of
+                                Right (Hello interval) -> do
+                                    -- All good, signal that the UDP thread can be started, and 
+                                    -- start the continuous event stream.
+                                    writeChan receives $ Right (Ready payload)
+                                    startEternalStream (ConnData conn info receives) interval 0 sends log
+                             
+                        Right m -> do
+                            writeChan log $ "Voice Websocket error - first message has to be opcode 4 Ready"
+                            writeChan receives $ Left $ VoiceWebsocketUnexpected m "Response to connection has to be Ready"
+                            pure ConnClosed
+                        Left m -> do
+                            writeChan log $ "Voice Websocket error - unexpected response"
+                            writeChan receives $ Left $ VoiceWebsocketUnexpected m "Response to connecting unexpected"
+                            pure ConnClosed
+                case next :: Either SomeException ConnLoopState of
+                    Left _ -> do
+                        writeChan log $ "Voice Websocket error - could not connect due to an exception"
+                        writeChan receives $ Left $ VoiceWebsocketConnection "Exception while connecting"
+                        pure ConnClosed
+                    Right n -> loop n 0
+
+            ConnReconnect -> do
+                next <- try $ connect (endpoint info) $ \conn -> do
+                    -- Send opcode 7 Resume
+                    sendTextData conn $ encode $ Resume (guildId connInfo) (sessionId connInfo) (token connInfo)
+                    -- Attempt to get opcode 9 Resumed
+                    msg <- getPayload conn log
+                    case msg of
+                        Right Resumed -> do
+                            startEternalStream (ConnData conn info receives) interval sends log
+                        Right m -> do
+                            writeChan log $ "Voice Websocket error - first message after resume should be opcode 9 Resumed"
+                            writeChan receives $ Left $ VoiceWebsocketUnexpected m "Response to resumption wrong"
+                            pure ConnClosed
+                        Left m -> do
+                            writeChan log $ "Voice Websocket error - unexpected response"
+                            writeChan receives $ Left $ VoiceWebsocketUnexpected m "Response to resumption unexpected"
+                            pure ConnClosed
+                case next :: Either SomeException ConnLoopState of
+                    Left _ -> do
+                        writeChan log $ "Voice Websocket error - could not resume, retrying after 10 seconds"
+                        threadDelay $ 10 * (10^(6 :: Int))
+                        loop ConnReconnect (retries + 1)
+                    Right n -> loop n 1
+
+getPayload :: Connection -> Chan T.Text -> IO (Either ConnectionException VoiceWebsocketReceivable)
+getPayload conn log = try $ do
+    msg' <- receiveData conn
+    case eitherDecode msg' of 
+        Right msg -> pure msg
+        Left err  -> do
+            writeChan log $ "Voice Websocket parse error - " <> T.pack err
+                <> " while decoding " <> TE.decodeUtf8 (BL.toStrict msg')
+            pure $ ParseError $ T.pack err
+
+getPayloadTimeout :: Connection -> Int -> Chan T.Text -> IO (Either ConnectionException VoiceWebsocketReceivable)
+getPayloadTimeout conn interval log = do
+  res <- race (threadDelay ((interval * 1000 * 3) `div` 2))
+              (getPayload conn log)
+  case res of
+    Left () -> pure (Right Reconnect) -- TODO stuff with reconnection and connectcode and stuff
+    Right other -> pure other
+
+-- | Create the sendable and heartbeat loops.
+startEternalStream :: WebsocketConnInfo -> Integer -> Chan VoiceWebsocketSendable -> Chan T.Text -> IO ConnLoopState
+startEternalStream connData interval sends log = do
+    writeChan log "Voice Websocket - eternal event stream started"
+    let err :: SomeException -> IO ()
+        err e = do
+            writeChan log $ "Voice Websocket error - event stream error: " <> T.pack (show e)
+            pure Reconnect
+    handle err $ do
+        sysSends <- newChan -- Chan for Heartbeat among other library-initiated sends
+        sendLoopId <- forkIO $ sendableLoop connData sysSends sends
+        heartLoopId <- forkIO $ heartbeatLoop connData sysSends interval
+
+        finally (eventStream connData interval sysSends) $
+            (killThread heartLoopId >> killThread sendLoopId)
+
+eventStream :: WebsocketConnData -> Integer -> Chan VoiceWebsocketSendable -> IO ConnLoopState
+eventStream connData interval sysSends = loop
+  where
+    loop :: IO ConnLoopState
+    loop = do
+        eitherPayload <- getPayloadTimeout (connection connData) interval log
+        case eitherPayload of
+            -- https://hackage.haskell.org/package/websockets-0.12.7.2/docs/Network-WebSockets.html#t:ConnectionException
+            Left (CloseRequest code str) -> do
+                writeChan log $ "Voice Websocket error - connection closed with code: " <> T.pack (show code)
+                pure ConnClosed
+            Left _ -> do
+                writeChan log $ "Voice Websocket error - connection exception in eventStream."
+                pure Reconnect
+            Right receivable ->
+                writeChan (eventChan connData) (Right receivable)
+                loop
+        
+
+-- | Eternally send data from sysSends and usrSends channels
+sendableLoop :: WebsocketConnData -> Chan VoiceWebsocketSendable -> Chan VoiceWebsocketSendable -> IO ()
+sendableLoop connData sysSends usrSends = do
+    -- Wait-time taken from discord-haskell/Internal.Gateway.EventLoop
+    threadDelay $ round ((10^(6 :: Int)) * (62 / 120) :: Double)
+    -- Get whichever possible, and send it
+    let choose :: Either VoiceWebsocketSendable VoiceWebsocketSendable -> VoiceWebsocketSendable
+        choose = either id id
+    payload <- choose <$> race (readChan sysSends) (readChan usrSends)
+    sendTextData (connection connData) $ encode payload
+    sendableLoop
+
+heartbeatLoop :: WebsocketConnData -> Chan VoiceWebsocketSendable -> Integer -> IO ()
+heartbeatLoop connData sysSends interval = do
+    threadDelay $ 1 * 10^(6 :: Int)
+    forever $ do
+        time <- round <$> getPOSIXTime
+        writeChan sysSends $ Heartbeat $ time
+        threadDelay $ interval * 1000
