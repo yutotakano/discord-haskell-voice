570ec277efa4c44046de68b5c05d21eefcd36113
Yuto Takano <moa17stock@gmail.com>
2021-06-06 21:14:23 +0100

    Make JoinSpecificVC more legible

--- examples/JoinSpecificVC.hs
+++ examples/JoinSpecificVC.hs
@@ -22,7 +22,7 @@ main = do
     tok <- TIO.readFile "./examples/auth-token.secret"
 
     t <- runDiscord $ def { discordToken = tok
-                          , discordOnStart = startHandler
+                          , discordOnStart = pure ()
                           , discordOnEnd = liftIO $ putStrLn "Ended"
                           , discordOnEvent = eventHandler
                           , discordOnLog = \s -> TIO.putStrLn s
@@ -31,25 +31,24 @@ main = do
 
 -- | Literally just joins this specific one and chills. lol.
 eventHandler :: Event -> DiscordHandler ()
-eventHandler event =
-    case event of
-        MessageCreate msg ->
-            case messageText msg of
-                "---joinVC" -> do
-                    eVc <- joinVoice (read "765660106259562498") (read "765660106259562502") False False
-                    -- eVc <- joinVoice (read "768810076201811989") (read "768810076201811993") False False
-                    case eVc of
-                        Left e -> do
-                            _ <- restCall $ R.CreateMessage (messageChannel msg) $
-                                (T.pack $ "Couldn't join!" <> show e)
-                            pure ()
-                        Right vc -> do
-                            contents <- liftIO $ BL.readFile "./music.raw"
-                            playPCM vc contents
-                            leaveVoice vc
-                            pure ()
-                _ -> pure ()
-        _ -> pure ()
+eventHandler (MessageCreate msg) =
+    case messageText msg of
+        "-joinVC" -> playSomeMusic msg
+        _         -> pure ()
+eventHandler _ = pure ()
 
-startHandler :: DiscordHandler ()
-startHandler = pure ()
+playSomeMusic :: Message -> DiscordHandler ()
+playSomeMusic msg = do
+    eVc <- joinVoice (read "765660106259562498") (read "765660106259562502") False False
+    -- eVc <- joinVoice (read "768810076201811989") (read "768810076201811993") False False
+    case eVc of
+        Left e -> do
+            _ <- restCall $ R.CreateMessage (messageChannel msg) $
+                (T.pack $ "Couldn't join!" <> show e)
+            pure ()
+        Right vc -> do
+            contents <- liftIO $ BL.readFile "./music.raw"
+            playPCM vc contents
+            leaveVoice vc
+            pure ()
+
