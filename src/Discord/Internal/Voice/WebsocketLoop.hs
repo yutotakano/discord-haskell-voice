e48d1b86a7783e35948455f30f87fb8c526772bd
Yuto Takano <moa17stock@gmail.com>
2021-12-28 16:10:00 +0000

    Allow (2 x heartbeat interval) tolerance in waiting for next packet

--- src/Discord/Internal/Voice/WebsocketLoop.hs
+++ src/Discord/Internal/Voice/WebsocketLoop.hs
@@ -279,8 +279,9 @@ eventStream
 eventStream conn opts interval udpLaunchOpts libSends log = do
     -- there has to be at least one packet every @interval@ milliseconds (which
     -- is the hearbeat response), so if we don't get that, it's a sign of
-    -- the connection gone, we should reconnect.
-    payload <- doOrTimeout interval $ getPayload conn
+    -- the connection gone, we should reconnect. For a quick heuristic accounting
+    -- for any network delays, allow for a tolerance of double the time.
+    payload <- doOrTimeout (interval * 2) $ getPayload conn
     print $ "(recv) " <> show payload -- TODO: debug, remove.
     case payload of
         Nothing -> do
