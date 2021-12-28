106211f8b0ff5217b58d009501f9f9369aa7caae
Yuto Takano <moa17stock@gmail.com>
2021-12-28 22:02:30 +0000

    Rewrite JoinAllVC using the new functions

--- examples/JoinAllVC.hs
+++ examples/JoinAllVC.hs
@@ -2,14 +2,15 @@ module Main where
 
 import           Control.Monad              ( forM_
                                             )
+import           Control.Monad.Trans        ( lift )
 import qualified Data.Text.IO as TIO
 import           Discord
--- import           Discord.Voice              ( joinVoice
---                                             )
+import           Discord.Voice
 import qualified Discord.Requests as R
 import           Discord.Types
 import           UnliftIO                   ( liftIO
                                             )
+import Control.Concurrent
 
 main :: IO ()
 main = do
@@ -19,7 +20,7 @@ main = do
                           , discordOnStart = startHandler
                           , discordOnEnd = liftIO $ putStrLn "Ended"
                           , discordOnEvent = eventHandler
-                          , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
+                          , discordOnLog = \s -> TIO.putStrLn s
                           }
     putStrLn "Finished!"
 
@@ -27,18 +28,22 @@ eventHandler :: Event -> DiscordHandler ()
 eventHandler event = pure ()
 
 startHandler :: DiscordHandler ()
-startHandler = pure ()
-    -- Right partialGuilds <- restCall R.GetCurrentUserGuilds
+startHandler = do
+    Right partialGuilds <- restCall R.GetCurrentUserGuilds
 
-    -- forM_ partialGuilds $ \pg -> do
-    --     Right guild <- restCall $ R.GetGuild (partialGuildId pg)
-    --     Right chans <- restCall $ R.GetGuildChannels (guildId guild)
-    --     case filter isVoiceChannel chans of
-    --         (c:_) -> do
-    --             _ <- joinVoice (guildId guild) (channelId c) False False
-    --             pure ()
-    --         _ -> pure ()
+    result <- runVoice $ do
+        forM_ partialGuilds $ \pg -> do
+            Right guild <- liftDiscord $ restCall $ R.GetGuild (partialGuildId pg)
+            Right chans <- liftDiscord $ restCall $ R.GetGuildChannels (guildId guild)
 
+            case filter isVoiceChannel chans of
+                (c:_) -> join (guildId guild) (channelId c)
+                _     -> pure ()
+
+        -- sit around for 30 seconds
+        liftIO $ threadDelay 30000000
+    liftIO $ print result
+    pure ()
 
 isTextChannel :: Channel -> Bool
 isTextChannel (ChannelText {}) = True
