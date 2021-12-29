2e1c422ee6ef07d82ceac09196d06018f4ef3958
Yuto Takano <moa17stock@gmail.com>
2021-12-29 17:27:00 +0000

    Add some Haddock documentation in Discord.Voice module

--- src/Discord/Voice.hs
+++ src/Discord/Voice.hs
@@ -1,6 +1,57 @@
+{-|
+Module      : Discord.Voice
+Description : Voice support for discord-haskell!
+Copyright   : (c) Yuto Takano (2021)
+License     : MIT
+Maintainer  : moa17stock@email.com
+
+Welcome to @discord-haskell-voice@! This library provides you with a high-level
+interface for interacting with Discord's Voice API, building on top of the
+@[discord-haskell](https://hackage.haskell.org/package/discord-haskell)@ library
+by Karl.
+
+For a quick intuitive introduction to what this library enables you to do, see
+the following snippet of code:
+
+@
+rickroll :: 'Channel' -> 'DiscordHandler' ()
+rickroll ChannelVoice {} = do
+    result <- runVoice $ do
+        join (channelGuild c) (channelId c)
+        playYouTube \"https:\/\/www.youtube.com\/watch?v=dQw4w9WgXcQ\"
+
+    case result of
+        Left err -> liftIO . print (show err) -- something failed...
+        Right _  -> pure () -- everything went well!
+@
+
+We can see that this library introduces a dedicated monad for voice operations,
+which opaquely guarantees that you won't accidentally keep hold of a closed
+voice connection, or try to use it after a network error had occurred.
+
+You'll also see further down the docs, that you can use
+[conduit](https://hackage.haskell.org/package/conduit) to stream arbitrary
+ByteString data as audio, as well as manipulate and transform streams using its
+interface. This is quite a powerful feature!
+
+Let's dive in :)
+-}
 module Discord.Voice
-    ( module Discord.Internal.Voice
+    ( 
+      -- * Monad for Voice Operations
+      Voice
+    , runVoice
+    , liftDiscord
+      -- * Operations you can do in Voice
+    , join
+    , updateSpeakingStatus
+    , play
+    , playPCMFile
+    , playFile
+    , playFileWith
+    , playYouTube
+    , defaultFFmpegArgs
     ) where
 
+import Discord.Internal.Types.VoiceCommon
 import Discord.Internal.Voice
-
