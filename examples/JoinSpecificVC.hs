c778d3046aba20d06f4ee70db906bd3b6477fbf0
Yuto Takano <moa17stock@gmail.com>
2021-12-31 12:24:25 +0000

    Add volume adjustment to JoinSpecificVC

--- examples/JoinSpecificVC.hs
+++ examples/JoinSpecificVC.hs
@@ -1,38 +1,38 @@
 module Main where
 
-import           Control.Concurrent         ( threadDelay
-                                            , MVar
-                                            , newMVar
-                                            , readMVar
-                                            , swapMVar
-                                            )
+import           Conduit
+import           Control.Concurrent.STM.TVar
 import           Control.Monad              ( when
                                             , guard
                                             , void
+                                            , forever
                                             )
-import qualified Data.ByteString.Lazy as BL
-import           Data.List                  ( intercalate )
+import           Data.List                  ( intercalate
+                                            )
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
--- import           Discord.Voice              ( joinVoice   -- <-- This is the magic!
---                                             , leaveVoice
---                                             , playYTDL
---                                             , DiscordVoiceHandle(..)
---                                             )
+import qualified StmContainers.Map as M
 import qualified Discord.Requests as R
 import           Discord.Types
+import           Discord.Voice
 import           Discord
 import           Options.Applicative
-import qualified StmContainers.Map as Map
 import           UnliftIO                   ( liftIO
                                             , atomically
                                             )
 
 data BotAction
-    = JoinVoice GuildId ChannelId
+    = JoinVoice ChannelId
     | LeaveVoice ChannelId
-    | PlayInVoice ChannelId [String]
+    | PlayInVoice String
+    | ChangeVolume Int
     deriving ( Read )
+
+data GuildContext = GuildContext
+    { songQueries :: [String]
+    , volume      :: TVar Int -- volume
+    , leaveFunc   :: Voice () -- function to leave voice channel
+    }
 
 parser :: ParserInfo BotAction
 parser = info
@@ -40,17 +40,19 @@ parser = info
         ( command "join"
             ( flip info (progDesc "Join a voice channel") $
                 JoinVoice <$>
-                argument auto (metavar "GUILDID" <> help "Guild Id") <*>
                 argument auto (metavar "CHANID" <> help "Voice Channel ID"))
         <> command "leave"
             ( flip info (progDesc "Leave a voice channel") $
                 LeaveVoice <$>
                 argument auto (metavar "CHANID" <> help "Voice Channel ID"))
-        <> command "play"
+        <> command "queue"
             ( flip info (progDesc "Play something!") $
-                PlayInVoice <$>
-                argument auto (metavar "CHANID" <> help "Voice Channel ID") <*>
+                PlayInVoice . intercalate " " <$>
                 some (argument str (metavar "QUERY" <> help "Search query/URL")))
+        <> command "volume"
+            ( flip info (progDesc "Change the volume for this server!") $
+                ChangeVolume <$>
+                argument auto (metavar "VOLUME" <> help "Integer volume"))
         )
     ) fullDesc
 
@@ -58,50 +60,91 @@ main :: IO ()
 main = do
     tok <- TIO.readFile "./examples/auth-token.secret"
 
+    queries <- M.newIO
     t <- runDiscord $ def
         { discordToken = tok
         , discordOnStart = pure ()
         , discordOnEnd = liftIO $ putStrLn "Ended"
-        , discordOnEvent = eventHandler
+        , discordOnEvent = eventHandler queries
         , discordOnLog = \s -> TIO.putStrLn s
         }
     putStrLn "Exiting..."
 
-eventHandler :: Event -> DiscordHandler ()
--- eventHandler voiceHandles (MessageCreate msg) = do
---     let args = map T.unpack $ T.words $ messageText msg
-    -- when (head args == "bot") $ do
-    --     let result = execParserPure defaultPrefs parser $ tail args
-    --     case result of
-    --         Success (JoinVoice gid cid) -> do
-    --             isInAlready <- liftIO $ atomically $ Map.lookup (show cid) voiceHandles
-    --             case isInAlready of
-    --                 Just _ ->
-    --                     void $ restCall $ R.CreateMessage (messageChannel msg) $
-    --                         "I'm already in that VC!"
-    --                 Nothing -> do
-    --                     eVc <- joinVoice gid cid False False
-    --                     case eVc of
-    --                         Left e ->
-    --                             void $ restCall $ R.CreateMessage (messageChannel msg) $
-    --                                 (T.pack $ "Couldn't join!" <> show e)
-    --                         Right vc ->
-    --                             liftIO $ atomically $ Map.insert vc (show cid) voiceHandles
-    --         Success (LeaveVoice cid) -> do
-    --             isInAlready <- liftIO $ atomically $ Map.lookup (show cid) voiceHandles
-    --             case isInAlready of
-    --                 Just vcHandle -> do
-    --                     leaveVoice vcHandle
-    --                     liftIO $ atomically $ Map.delete (show cid) voiceHandles
-    --                 _ -> pure ()
-    --         Success (PlayInVoice cid q) -> do
-    --             isInAlready <- liftIO $ atomically $ Map.lookup (show cid) voiceHandles
-    --             case isInAlready of
-    --                 Just vcHandle ->
-    --                     playYTDL vcHandle (intercalate " " q) "youtube-dl" "ffmpeg"
-    --                 _ -> pure ()
-    --         Failure failure ->
-    --             void $ restCall $ R.CreateMessage (messageChannel msg) $ T.pack $
-    --                 fst $ renderFailure failure "bot"
-eventHandler _ = pure ()
+eventHandler :: M.Map String GuildContext -> Event -> DiscordHandler ()
+eventHandler contexts (MessageCreate msg) = do
+    let args = map T.unpack $ T.words $ messageText msg
+    case args of
+        ("bot":_) -> case (execParserPure defaultPrefs parser $ tail args) of
+            Success x -> handleCommand contexts msg x
+            Failure failure ->
+                void $ restCall $ R.CreateMessage (messageChannel msg) $ T.pack $
+                    fst $ renderFailure failure "bot"
+        _ -> pure ()
+eventHandler _ _ = pure ()
 
+handleCommand :: M.Map String GuildContext -> Message -> BotAction -> DiscordHandler ()
+handleCommand contexts msg (JoinVoice cid) =
+    -- Check if the message is sent in a server.
+    case messageGuild msg of
+        Nothing  -> pure ()
+        Just gid -> do
+            result <- runVoice $ do
+                leave <- join gid cid
+                volume <- liftIO $ newTVarIO 100
+                liftDiscord $ atomically $ M.insert (GuildContext [] volume leave) (show gid) contexts
+                -- Forever, read the top of the queue and play it.
+                forever $ do
+                    context <- liftDiscord $ atomically $ M.lookup (show gid) contexts
+                    case context of
+                        Nothing -> pure ()
+                        Just (GuildContext [] _ _) -> pure ()
+                        Just (GuildContext (x:xs) _ _) -> do
+                            liftDiscord $ atomically $ M.insert (GuildContext xs volume leave) (show gid) contexts
+                            let adjustVolume = awaitForever $ \current -> do
+                                    v' <- liftIO $ readTVarIO volume
+                                    yield $ round (fromIntegral current * (fromIntegral v' / 100))
+                            playYouTube' x $ packTo16C .| adjustVolume .| packFrom16C 
+
+            case result of
+                Left e -> liftIO $ print e >> pure ()
+                Right _ -> pure ()
+
+handleCommand contexts msg (LeaveVoice cid) =
+    case messageGuild msg of
+        Nothing -> pure ()
+        Just gid -> do
+            context <- atomically $ M.lookup (show gid) contexts
+            case context of
+                Nothing -> pure ()
+                Just (GuildContext _ _ leave) -> do
+                    void $ atomically $ M.delete (show gid) contexts
+                    void $ runVoice leave
+
+handleCommand contexts msg (PlayInVoice q) =
+    case messageGuild msg of
+        Nothing -> pure ()
+        Just gid -> do
+            resultQueue <- atomically $ do
+                context <- M.lookup (show gid) contexts
+                case context of
+                    Nothing -> do
+                        volume <- newTVar 100
+                        M.insert (GuildContext [q] volume (pure ())) (show gid) contexts
+                        pure [q]
+                    Just (GuildContext xs v leave) -> do
+                        M.insert (GuildContext (xs ++ [q]) v leave) (show gid) contexts
+                        pure $ xs ++ [q]
+            void $ restCall $ R.CreateMessage (messageChannel msg) $
+                (T.pack $ "Queued for playback: " <> show resultQueue)
+
+handleCommand contexts msg (ChangeVolume amount) =
+    case messageGuild msg of
+        Nothing -> pure ()
+        Just gid -> do
+            context <- atomically $ M.lookup (show gid) contexts
+            case context of
+                Nothing -> pure ()
+                Just (GuildContext q v l) -> do
+                    atomically $ swapTVar v amount
+                    void $ restCall $ R.CreateMessage (messageChannel msg) $
+                        (T.pack $ "Volume set to " <> show amount)
