a965eee7c557b339e58702d9db2de653cbe48aef
Yuto Takano <moa17stock@gmail.com>
2022-10-19 01:20:37 +0200

    Add static runtime analysis comment for 348fe29

--- src/Discord/Internal/Voice/OggParser.hs
+++ src/Discord/Internal/Voice/OggParser.hs
@@ -128,17 +128,25 @@ oggPageExtractC = loop BL.empty
                     loop rest
 
 -- | Conduit to extract the Opus bytes from an Ogg Page. This also handles the
--- addition of empty frames when there is no audio.
+-- addition of empty frames when there is no audio. The output of this conduit
+-- is a strict bytestring since there is no longer any need for laziness, and
+-- it's better to make it strict when we can guarantee properties of it (like
+-- that each packet will only be made strict once) than later down when we know
+-- for sure we need a strict bytestring anyway to send to Discord.
 opusPacketExtractC :: ConduitT OggPage BS.ByteString IO ()
-opusPacketExtractC = loop BS.empty
+opusPacketExtractC = loop BL.empty
   where
-    loop :: BS.ByteString -> ConduitT OggPage BS.ByteString IO ()
+    loop :: BL.ByteString -> ConduitT OggPage BS.ByteString IO ()
     loop opusSegment = do
         mbPage <- await
         case mbPage of
             Nothing -> do
-                -- Send any incomplete leftovers
-                if BS.null opusSegment then pure () else yield opusSegment
+                -- Send any incomplete leftovers. The toStrict here is
+                -- run on the remaining unterminated Opus packet, which was never
+                -- called toStrict upon previously. This is in sync with the
+                -- runtime explanation given in splitOggPackets (that toStrict
+                -- is only called maximally once for each segment).
+                if BL.null opusSegment then pure () else yield (BL.toStrict opusSegment)
 
                 -- Send at least 5 blank frames (20ms * 5 = 100 ms)
                 let enCfg = mkEncoderConfig opusSR48k True app_audio
@@ -149,7 +157,7 @@ opusPacketExtractC = loop BS.empty
                     yield encoded
             Just (OggPage hdr segsBytes) -> do
                 let (pkts, untermSeg, _) = splitOggPackets (oggPageSegmentLengths hdr) segsBytes opusSegment
-                forM_ pkts yield
+                forM_ pkts $ yield . BL.toStrict
                 loop untermSeg
 
 -- | Conduit to filter out any packets beginning with OpusHead or OpusTag.
@@ -181,12 +189,12 @@ splitOggPackets
     -> BS.ByteString
     -- ^ The bytes. Unless the network packet itself was corrupt or lost halfway,
     -- this should have at least the total length of all segments.
-    -> BS.ByteString
+    -> BL.ByteString
     -- ^ Any previous segment data that was not terminated in the previous page.
-    -> ([BS.ByteString], BS.ByteString, BS.ByteString)
+    -> ([BL.ByteString], BL.ByteString, BS.ByteString)
     -- ^ A tuple containing: (the list of parsed and concatted packets, any
     -- unterminated segments, any leftover bytes that were after the segments).
-splitOggPackets sl bytes ps = loop sl bytes (BL.fromStrict ps)
+splitOggPackets sl bytes ps = loop sl bytes ps
   where
     loop
         :: [Int]
@@ -196,34 +204,63 @@ splitOggPackets sl bytes ps = loop sl bytes (BL.fromStrict ps)
         -> BL.ByteString
         -- ^ Any previous unterminated segments that need to be prepended with
         -- this one to construct a complete Opus packet. This argument is lazy
-        -- since it can undergo an append operation on each loop. If we use
-        -- strict bytestrings here, in the worst case when all segments within
-        -- an Ogg Page are 255 long, there will be 0 byte copies at the first
-        -- iteration, 255*2 at the second iteration (strict append has O(m+n)),
-        -- 255*3 at third etc. If we use lazy bytestrings, then there will be
-        -- 0 byte copies at the first, 255 at the second (lazy append has O(m)),
-        -- 255*2 at second etc. In ffmpeg outputs set to what Discord wants,
-        -- an Opus packet spans more than one segment, usually two to three. We
-        -- can therefore benefit from laziness.
+        -- since it can undergo an append operation on each loop.
         --
-        -- The toStrict operations are costly. They are a temporary necessity
-        -- until the return type of 'splitOggPackets' is lazy (which will
-        -- happen soon).
-        -> ([BS.ByteString], BS.ByteString, BS.ByteString)
-    loop [] bytes previousSegment = ([], BL.toStrict previousSegment, bytes)
+        -- To be precise:
+        --
+        -- 1. Both Lazy and Strict bytestring variants would need to be made
+        --    strict in its entirety once: O(n). The lazy variant will be made
+        --    strict at the very end, while the strict one will be made strict
+        --    before calling splitOggPackets.
+        -- 2. Both Lazy and Strict bytestring variants have O(1) operation on
+        --    self-contained segments (i.e. Opus packets that don't span multiple
+        --    segments). For Lazy, we use constant-time strict splitAt: O(1);
+        --    then convert the strict bytestring to lazy: O(1); and prepend that
+        --    to the result list: O(1). For Strict, we use the same splitAt
+        --    operation: O(1); And just prepend it to the result list: O(1).
+        -- 3. Both Lazy and Strict bytestring variants have
+        --    O(255 * (n * (n + 1) / 2)) for n segments that continue onto the
+        --    next. In the case of Lazy, the discovery of a new nth continuation
+        --    segment n will perform an append operation walking through the
+        --    spine of previous (n - 1) * 255 bytes. The discovery of a
+        --    termination segment at n+1 will walk through n * 255 bytes. This
+        --    totals to (n * (n + 1) / 2) times walkthrough of 255 bytes.
+        --    In the case of Strict, the discovery of a new nth continuation
+        --    segment n will perform an append operation walking through both
+        --    the previous (n - 1) * 255 bytes and the new 255 bytes, so n * 255.
+        --    This totals to (n * (n + 1) / 2) times walkthrough of 255 bytes,
+        --    although we have yet to consider the termination segment.
+        -- 4. The discovery of a termination segment at n+1 in the Strict variant
+        --    incurs a further walkthrough of the previous 255 * n bytes, plus
+        --    however many bytes are in the last segment (<255).
+        --
+        -- Because 4 introduces an additional factor of 255 * n + lastBytes, the
+        -- Lazy variant is preferred.
+        -> ([BL.ByteString], BL.ByteString, BS.ByteString)
+    loop [] bytes previousSegment = ([], previousSegment, bytes) -- O(255i)
     loop (segLen : segLengths) bytes previousSegment
-        -- Handle the case when there is at least another segment after this in
-        -- the same packet
-        | (seg, rest) <- BS.splitAt (segLen) bytes
-        , BS.length seg == segLen
-        , segLen == 255
-        = loop segLengths rest (previousSegment <> BL.fromStrict seg)
-        -- Handle the case when this segment terminates a packet (or is self-
-        -- conclusive)
-        | (seg, rest) <- BS.splitAt (segLen) bytes
-        , BS.length seg == segLen
-        , segLen < 255
-        = _1 %~ (BL.toStrict (previousSegment <> BL.fromStrict seg) :) $ loop segLengths rest BL.empty
+        -- Handle the case when this is the first segment in a series of more
+        -- than one segments.
+        | (seg, rest) <- BS.splitAt segLen bytes -- O(1)
+        , BS.length seg == segLen -- O(1)
+        , segLen == 255 -- O(1)
+        , BL.null previousSegment -- O(1)
+        = loop segLengths rest $ BL.fromStrict seg -- O(1)
+        | (seg, rest) <- BS.splitAt segLen bytes -- O(1)
+        , BS.length seg == segLen -- O(1)
+        , segLen == 255 -- O(1)
+        = loop segLengths rest (previousSegment <> BL.fromStrict seg) -- O(255 * m), where m is number of previous segments
+        -- Handle the case when this segment is self-conclusive
+        | (seg, rest) <- BS.splitAt segLen bytes -- O(1)
+        , BS.length seg == segLen -- O(1)
+        , segLen < 255 -- O(1)
+        , BL.null previousSegment -- O(1)
+        = _1 %~ (BL.fromStrict seg :) $ loop segLengths rest BL.empty -- O(1)
+        -- Handle the case when this segment terminates a packet
+        | (seg, rest) <- BS.splitAt segLen bytes -- O(1)
+        , BS.length seg == segLen -- O(1)
+        , segLen < 255 -- O(1)
+        = _1 %~ (previousSegment <> BL.fromStrict seg :) $ loop segLengths rest BL.empty -- O(255 * m) + recursion, where m is number of previous segments
         -- Handle the case when the segment length doesn't match!! This only
         -- happens if the network packet was lost somewhere, or if the Ogg packet
         -- is simply corrupt.
